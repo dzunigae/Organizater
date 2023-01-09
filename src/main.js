09876b0a4e630f5050f5d21691db0c678c35beb7
Daniel Zuñiga <33994323+dzunigae@users.noreply.github.com>
2023-01-09 16:40:53 -0500

    Adding electron js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+//import { createApp } from "vue";
+//import App from "./App.vue";
 
-createApp(App).mount('#app')
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile("./App.vue");
+});
+
+//createApp(App).mount("#app");
